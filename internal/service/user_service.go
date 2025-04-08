f45fa469b49ce692707d3a39ba36f01b3dcdc083
Tena <cestovlolo735@gmail.com>
2025-04-08 20:45:27 +0500

    service layer

---
+++ internal/service/user_service.go
@@ -0,0 +1,42 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"github.com/go-playground/validator/v10"
+	"users/internal/model"
+	"users/internal/repository"
+)
+
+type UserService interface {
+	CreateUser(ctx context.Context, user model.UserCreate) (model.User, error)
+	GetUser(ctx context.Context, id string) (model.User, error)
+	UpdateUser(ctx context.Context, id string, user model.UserUpdate) (model.User, error)
+	DeleteUser(ctx context.Context, id string) error
+}
+
+type userService struct {
+	userRepo  repository.UserRepository
+	validator *validator.Validate
+}
+
+func NewUserService(userRepo repository.UserRepository) UserService {
+	return &userService{
+		userRepo:  userRepo,
+		validator: validator.New(),
+	}
+}
+func (s userService) CreateUser(ctx context.Context, userCreate model.UserCreate) (model.User, error) {
+	if err := s.validator.Struct(userCreate); err != nil {
+		return model.User{}, err
+	}
+	return s.userRepo.Create(ctx, userCreate)
+}
+
+func (s userService) GetUser(ctx context.Context, id string) (model.User, error) {
+	if id == "" {
+		return model.User{}, errors.New("id is required")
+	}
+
+	return s.userRepo.GetByID(ctx, id)
+}
